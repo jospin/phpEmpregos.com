28486d397354a78102a8e4998faa0b47dfb96ac3
Mathias Grimm <mathiasgrimm@gmail.com>
2015-03-12 10:54:27 +0000

    adding base structure for publishing jobs on social networks

---
+++ phpEmpregos/SocialPublisher/LinkedIn/LinkedInPublisher.php
@@ -0,0 +1,11 @@
+<?php namespace phpEmpregos\SocialPublisher\Facebook;
+
+use phpEmpregos\SocialPublisher\SocialPublisher;
+
+class LinkedInPublisher implements SocialPublisher
+{
+    public function publish($jobText)
+    {
+
+    }
+}
---
+++ phpEmpregos/SocialPublisher/Twitter/TwitterPublisher.php
@@ -0,0 +1,11 @@
+<?php namespace phpEmpregos\SocialPublisher\Facebook;
+
+use phpEmpregos\SocialPublisher\SocialPublisher;
+
+class TwitterPublisher implements SocialPublisher
+{
+    public function publish($jobText)
+    {
+
+    }
+}
